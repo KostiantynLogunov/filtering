ec8f59bc022e21132fae7668c1394666fb5579ab
KostiantynLogunov <777koc@gmail.com>
2018-12-03 16:06:35 +0200

    fixed for SOLID

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,38 +3,36 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\UsersFilter;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
     public function index(Request $request)
     {
-
         $users = User::with('info');
 
-        if ($request->has('name'))
-        {
+        $users = (new UsersFilter($users, $request))->apply()->get();
+
+        /*if ($request->has('name')) {
             $users->where('name', 'like', "%$request->name%");
         }
 
-        if ($request->has('is_active'))
-        {
+        if ($request->has('is_active')) {
             $users->where('is_active', $request->is_active);
         }
 
-        if ( $request->has('birthday') )
-        {
+        if ( $request->has('birthday') ) {
             $users->whereHas('info',function ($query) use ($request){
                 $query->where('birthday', '>', $request->birthday);
             });
         }
 
-        if ($request->has('gender'))
-        {
+        if ($request->has('gender')) {
             $users->where('gender', $request->gender);
-        }
+        }*/
 
-        $users = $users->get();
+//        $users = $users->get();
 
         return view('users.index', compact('users'));
     }
